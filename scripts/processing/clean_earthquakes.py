e6b639fea9bcd2d8f1db7bd3f176ca85196280fb
Maryam-ghol <gholamimaryam@gmail.com>
2026-05-03 11:06:30 +0200

    change clean python file

--- scripts/processing/clean_earthquakes.py
+++ scripts/processing/clean_earthquakes.py
@@ -1,6 +1,7 @@
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
+
 from scripts.utils.logger import logger
 
 
@@ -10,17 +11,29 @@ RAW_DATA_PATH.mkdir(parents=True, exist_ok=True)
 PROCESSED_DATA_PATH = Path("data/processed")
 PROCESSED_DATA_PATH.mkdir(parents=True, exist_ok=True)
 
-def clean_earthquake_data(**context):
-    logger.info(f"Reading earthquakes dat from json file: {context['ti'].xcom_pull(task_ids='fetch_earthquake_data')}")
 
-    with open(context['ti'].xcom_pull(task_ids='fetch_earthquake_data'), "r") as f:
+def clean_earthquake_data(input_file=None, **context):
+
+    # 📌 If running inside Airflow → use XCom----“Get data returned from the fetch_earthquake_data task.”
+    if input_file is None:
+        input_file = context['ti'].xcom_pull(
+            task_ids='fetch_earthquake_data'
+        )
+
+    logger.info(f"Reading earthquake data from: {input_file}")
+
+    with open(input_file, "r") as f:
         data = json.load(f)
 
-    # Perform cleaning operations on the data
-    cleaned_data = [earthquake for earthquake in data['features'] if earthquake['properties']['mag'] is not None]
+    # ✅ Keep only earthquakes with valid magnitude
+    cleaned_data = [
+        earthquake
+        for earthquake in data['features']
+        if earthquake['properties']['mag'] is not None
+    ]
 
-    # Save the cleaned data
     filename = f"cleaned_earthquakes_{datetime.now().date()}.json"
+
     file_path = PROCESSED_DATA_PATH / filename
 
     with open(file_path, "w") as f:
@@ -30,6 +43,9 @@ def clean_earthquake_data(**context):
 
     return str(file_path)
 
-    
+
 if __name__ == "__main__":
-    clean_earthquake_data()
+
+    latest_file = sorted(RAW_DATA_PATH.glob("*.json"))[-1]
+
+    clean_earthquake_data(input_file=str(latest_file))
