d1e344a55d9ce96caf0b83eb98f37536502e8c57
Maryam-ghol <gholamimaryam@gmail.com>
2026-05-03 10:27:51 +0200

    Add logger

--- scripts/fetch/fetch_earthquakes.py
+++ scripts/fetch/fetch_earthquakes.py
@@ -2,22 +2,15 @@ import requests
 import json
 from datetime import datetime, timedelta
 from pathlib import Path
-
+from scripts.utils.logger import logger
 
 USGS_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query"
-
 
 RAW_DATA_PATH = Path("data/raw")
 RAW_DATA_PATH.mkdir(parents=True, exist_ok=True)
 
-
 def fetch_earthquake_data(**context):
-    """
-    Fetch earthquake data from USGS API for the last 1 day
-    and save it as a raw JSON file.
-    """
 
-    # 📅 Define time window (last 1 day)
     end_time = datetime.utcnow()
     start_time = end_time - timedelta(days=1)
 
@@ -28,7 +21,7 @@ def fetch_earthquake_data(**context):
         "minmagnitude": 0
     }
 
-    print(f"Fetching earthquakes from {params['starttime']} to {params['endtime']}")
+    logger.info(f"Fetching earthquakes from {params['starttime']} to {params['endtime']}")
 
     response = requests.get(USGS_URL, params=params)
 
@@ -37,15 +30,18 @@ def fetch_earthquake_data(**context):
 
     data = response.json()
 
-    # 🗂 Save file with timestamp
     filename = f"earthquakes_{start_time.date()}_{end_time.date()}.json"
     file_path = RAW_DATA_PATH / filename
 
     with open(file_path, "w") as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=4)
 
-    print(f"Saved raw data to: {file_path}")
+    logger.info(f"Saved raw data to: {file_path}")
 
     
-    
+
     return str(file_path)
+
+
+if __name__ == "__main__":
+    fetch_earthquake_data()
